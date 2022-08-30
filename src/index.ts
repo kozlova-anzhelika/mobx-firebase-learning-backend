7e8b12afd7030364e6add65aecb76d9b7161e70c
kozlovaangelika <a3.kozlova@itransition.com>
2022-08-30 17:01:14 +0300

    fix: remove mongodb and install mongose

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,15 @@
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import express from 'express';
-import { MongoClient } from 'mongodb';
+import mongoose from 'mongoose';
 import verifyEmailRouter from './routes/verifyEmail';
 import { logger } from './utils/constants';
 
 dotenv.config();
 
-const mongo = new MongoClient(
-  `mongodb://localhost:${process.env.REACT_APP_MONGO_PORT}/${process.env.REACT_APP_MONGO_DATA_BASE_NAME}`,
-);
 const app = express();
 const PORT = process.env.PORT || 5000;
+const databaseUri = `mongodb://localhost:${process.env.REACT_APP_MONGO_PORT}/${process.env.REACT_APP_MONGO_DATA_BASE_NAME}`;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -26,7 +24,7 @@ app.use((err: Error, req: express.Request, res: express.Response, next: express.
 
 async function start(): Promise<void> {
   try {
-    await mongo.connect();
+    await mongoose.connect(databaseUri);
     app.listen(PORT, () => {
       console.log(`Server has been started on port ${PORT}`);
     });
